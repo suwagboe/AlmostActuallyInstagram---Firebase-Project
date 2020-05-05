2b3ecb01cc67db146a8e48be1f84247d058b3223
uwagboe <sm.uwagboe@gmail.com>
2020-05-05 19:59:36 -0400

    add the function to get gain access to the photo with actionSheet

--- AlmostActuallyInstagram/controllers/UploadAPhotoViewController.swift
+++ AlmostActuallyInstagram/controllers/UploadAPhotoViewController.swift
@@ -10,13 +10,59 @@ import UIKit
 
 class UploadAPhotoViewController: UIViewController {
     
+    
+    @IBOutlet weak var butterflyImage: UIImageView!
     @IBOutlet weak var imageDescription: UITextField!
-    @IBOutlet weak var imageView: UIImageView!
+    
+    private lazy var imagePickerController: UIImagePickerController = {
+       let picker = UIImagePickerController()
+     //   picker.delegate = self
+        return picker
+    }()
+    
+    private lazy var longPressGesture: UILongPressGestureRecognizer = {
+        let gesture = UILongPressGestureRecognizer()
+        gesture.addTarget(self, action: #selector(showPhotoOptions))
+        return gesture
+    }()
 
+    private var selectedImage: UIImage? {
+        didSet {
+            butterflyImage.image = selectedImage
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    @objc private func showPhotoOptions() {
+        let alertController = UIAlertController(title: "Where is the photo", message: nil, preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "capture the moment live", style: .default) { (action) in
+            self.imagePickerController.sourceType = .camera
+            self.present(self.imagePickerController, animated: true)
+        }
+        
+        let photoLibrary = UIAlertAction(title: "look at your photos", style: .default) { (action) in
+            self.imagePickerController.sourceType = .photoLibrary
+            self.present(self.imagePickerController, animated: true)
+        }
+        
+        let cancelAction = UIAlertAction(title: "", style: .cancel)
+          
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            alertController.addAction(cameraAction)
+        }
+        
+        alertController.addAction(photoLibrary)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+        
+       }
+
+    
     
 
 
---
+++ AlmostActuallyInstagram/postModel.swift
@@ -0,0 +1,16 @@
+//
+//  postModel.swift
+//  AlmostActuallyInstagram
+//
+//  Created by Pursuit on 5/5/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Category {
+  let image: UIImage
+  let name: String
+    
+}
